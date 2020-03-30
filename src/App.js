4803b00595c40e975f8eb8d92e38f1ed4bbaeac0
Gavin Dreyer <gavindreyer1@gmail.com>
2020-03-30 10:55:59 -0700

    displaying current rooms information

--- src/App.js
+++ src/App.js
@@ -5,20 +5,46 @@ import { Queue, Stack } from './util';
 import './App.css';
 
 function App() {
+	const [currentRoom, setCurrentRoom] = useState();
+
 	useEffect(() => {
 		axios
 			.get('https://lambda-treasure-hunt.herokuapp.com/api/adv/init/', {
 				headers: { Authorization: process.env.REACT_APP_EXPLORER_TOKEN }
 			})
 			.then(res => {
-				console.log(res);
+				setCurrentRoom(res.data);
 			})
 			.catch(err => {
 				console.log(err);
 			});
 	}, []);
 
-	return <div className="App">Explorer</div>;
+	if (!currentRoom) {
+		return <div>loading...</div>;
+	}
+
+	return (
+		<div className="App">
+			<h3>Explorer</h3>
+			<div className="room">{currentRoom.room_id}</div>
+			<div className="room">{currentRoom.title}</div>
+			<div className="room">{currentRoom.description}</div>
+			<div className="room">{currentRoom.coordinates}</div>
+			<div className="room">{currentRoom.elevation}</div>
+			<div className="room">{currentRoom.terrain}</div>
+			<div className="room">{currentRoom.items}</div>
+			<div className="room">
+				{currentRoom.exits.map((exit, idx) => (
+					<span className="exits" key={idx}>
+						{exit}
+					</span>
+				))}
+			</div>
+			<div className="room">{currentRoom.errors}</div>
+			<div className="room">{currentRoom.messages}</div>
+		</div>
+	);
 }
 
 export default App;
