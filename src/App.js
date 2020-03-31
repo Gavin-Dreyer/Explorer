107272f12beeb7ccd41c85df7ce438bbf1eed0d8
Gavin Dreyer <gavindreyer1@gmail.com>
2020-03-31 09:39:15 -0700

    setting initial room information to state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-import { Queue, Stack } from './util';
+//import { Queue, Stack } from './util';
 import './App.css';
 
 function App() {
 	const [currentRoom, setCurrentRoom] = useState();
+	const [visited, setVisited] = useState([]);
+	const [stack, setStack] = useState([]);
+	const [connections, setConnections] = useState();
 
 	useEffect(() => {
 		axios
@@ -13,33 +16,68 @@ function App() {
 				headers: { Authorization: process.env.REACT_APP_EXPLORER_TOKEN }
 			})
 			.then(res => {
+				let allExits = [];
+				let rmConnections = {};
+
 				setCurrentRoom(res.data);
+				setVisited([...visited, res.data.room_id]);
+
+				res.data.exits.forEach(exit => {
+					allExits = [...allExits, [res.data.room_id, exit]];
+					rmConnections = { ...rmConnections, [`${exit}`]: '?' };
+				});
+
+				setStack(allExits);
+				setConnections({ [`${res.data.room_id}`]: rmConnections });
 			})
 			.catch(err => {
 				console.log(err);
 			});
 	}, []);
 
+	console.log(stack, connections);
 	useEffect(() => {
 		if (currentRoom) {
-			const interval = setInterval(() => {
-				axios
-					.post(
-						'https://lambda-treasure-hunt.herokuapp.com/api/adv/move/',
-						{ direction: 'n' },
-						{
-							headers: { Authorization: process.env.REACT_APP_EXPLORER_TOKEN }
-						}
-					)
-					.then(res => {
-						setCurrentRoom(res.data);
-					})
-					.catch(err => {
-						console.log(err);
-					});
-			}, 1000 * currentRoom.cooldown);
+			const dft = startingRoom => {
+				let visitedRms = new Set();
 
-			return () => clearInterval(interval);
+				console.log(stack, connections);
+
+				// while (stack.size() > 0) {
+				// let prevRm = startingRoom;
+				//let path = stack.pop();
+
+				// axios
+				// 	.post(
+				// 		'https://lambda-treasure-hunt.herokuapp.com/api/adv/move/',
+				// 		{ direction: path[1] },
+				// 		{
+				// 			headers: { Authorization: process.env.REACT_APP_EXPLORER_TOKEN }
+				// 		}
+				// 	)
+				// 	.then(res => {
+				// 		setCurrentRoom(res.data);
+				// 		if (path[1] === 'w') {
+				// 			connections[prevRm].w = currentRoom.room_id;
+				// 		} else if (path[1] === 'e') {
+				// 			connections[prevRm].e = currentRoom.room_id;
+				// 		} else if (path[1] === 'n') {
+				// 			connections[prevRm].n = currentRoom.room_id;
+				// 		} else {
+				// 			connections[prevRm].s = currentRoom.room_id;
+				// 		}
+				// 		setVisited([...visited, res.data.room_id]);
+				// 	})
+				// 	.catch(err => {
+				// 		console.log(err);
+				// 	});
+				//don't
+				//}
+			};
+			dft(currentRoom);
+
+			//setTimeout(dft, 1000 * currentRoom.cooldown, currentRoom);
+			//console.log(stack, connections);
 		}
 	}, [currentRoom]);
 
@@ -66,6 +104,7 @@ function App() {
 			</div>
 			<div className="room">{currentRoom.errors}</div>
 			<div className="room">{currentRoom.messages}</div>
+			<div>{visited}</div>
 		</div>
 	);
 }
